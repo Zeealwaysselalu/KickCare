2f35bb0bdc00873090a879ac5d6703d150009420
Zeeaalwaysselalu <zaelania733@gmail.com>
2026-04-05 19:16:25 +0700

    Laravel breeze reinstall

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
-use App\Mail\InvoiceMail;
-use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/bar', function () {
-    return view('testbarcode');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/send', function () {
-    Mail::to("aaaaa@gmail.com")->send(new InvoiceMail());
-});
+require __DIR__.'/auth.php';
